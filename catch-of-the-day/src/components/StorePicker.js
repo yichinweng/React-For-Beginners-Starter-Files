55f6c83c1fbf9e4d74aba6b1072788677d8900e3
Winter <winter@hopenglish-RDde-MacBook-Air.local>
2017-06-29 16:01:49 +0800

    ch.05 first own component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+	render() {
+		return (
+			<form className="store-selector">
+				{ /* 我是註解啊～ */}
+				<h2>Please Enter A Store</h2>
+				<input type="text" placeholder="Store Name" required />
+				<button type="submit">Visit Store >>></button>
+			</form>
+		)
+	}
+}
+
+export default StorePicker;
